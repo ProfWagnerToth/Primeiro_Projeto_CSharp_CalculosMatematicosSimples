6328384f0567aec4985e3c1a387023d3fbc8ec1c
Wagner Ramos Toth <103787298+wagnertothetec@users.noreply.github.com>
2022-07-06 16:06:48 -0300

    Add project files.

---
+++ Primeiro_Projeto_CSharp_CalculosMatematicosSimples/Form1.cs
@@ -0,0 +1,10 @@
+namespace Primeiro_Projeto_CSharp_CalculosMatematicosSimples
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
