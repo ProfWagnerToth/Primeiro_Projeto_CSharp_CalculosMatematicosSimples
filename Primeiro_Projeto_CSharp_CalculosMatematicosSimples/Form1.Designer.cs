a7784fcc850d601d28cb3073acec5663a1498561
Wagner Ramos Toth <103787298+wagnertothetec@users.noreply.github.com>
2022-07-06 16:13:19 -0300

    Projeto Básico das 4 opeções Matemáticas (Adição, Subtração, Multiplicação e Divisão)

--- Primeiro_Projeto_CSharp_CalculosMatematicosSimples/Form1.Designer.cs
+++ Primeiro_Projeto_CSharp_CalculosMatematicosSimples/Form1.Designer.cs
@@ -28,12 +28,187 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(205, 194);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(103, 23);
+            this.button4.TabIndex = 27;
+            this.button4.Text = "Divisão";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(96, 194);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(103, 23);
+            this.button3.TabIndex = 26;
+            this.button3.Text = "Multiplicação";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(205, 146);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 23);
+            this.button2.TabIndex = 25;
+            this.button2.Text = "Subtração";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(96, 146);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 23);
+            this.button1.TabIndex = 24;
+            this.button1.Text = "Adição";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(306, 86);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(79, 23);
+            this.textBox3.TabIndex = 23;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(306, 59);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 15);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "Resultado";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(187, 86);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(79, 23);
+            this.textBox2.TabIndex = 21;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(8, 86);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(79, 23);
+            this.textBox1.TabIndex = 20;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(269, 89);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(15, 15);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "=";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(119, 89);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 15);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Op";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(187, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 15);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "2 Numero";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(96, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 15);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Operação";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 15);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "1 Numero";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(96, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 15);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Meu Primeiro Projeto de Aplicação C#";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(393, 232);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private TextBox textBox3;
+        private Label label7;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
     }
 }
--- Primeiro_Projeto_CSharp_CalculosMatematicosSimples/Form1.cs
+++ Primeiro_Projeto_CSharp_CalculosMatematicosSimples/Form1.cs
@@ -6,5 +6,97 @@ namespace Primeiro_Projeto_CSharp_CalculosMatematicosSimples
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Declaração de Variáveis para realização dos calculos
+            double soma1;
+            double soma2;
+            double resultado;
+
+            //Recebendo valores informados pelo usuário na interface da aplicação
+
+            soma1 = double.Parse(textBox1.Text); // Recebe valor referente a textBox1 do formulário e converte uma informação tipo Texto para Double, pois a variavel é do tipo double, ou seja, pode receber numeros (Inteiros, Fracionados, Positivos ou Negativos)
+
+            soma2 = double.Parse(textBox2.Text); // Recebe valor referente a textBox2 do formulário e converte uma informação tipo Texto para Double, pois a variavel é do tipo double, ou seja, pode receber numeros (Inteiros, Fracionados, Positivos ou Negativos)
+
+            //Efetuando o calculo de Adição
+            resultado = soma1 + soma2; //Executa o Calculo
+
+            //Mostrar o resultado ao usuário
+            textBox3.Text = resultado.ToString(); //Converte o valor da variavel do tipo double, para texto e assim será possivel retornar o valor dentro uma textBox.
+
+            //Mostrar a operação matemática executada dentro da Label
+            label5.Text = " + ";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Declaração de Variáveis para realização dos calculos
+            double subtrai1;
+            double subtrai2;
+            double resultado;
+
+            //Recebendo valores informados pelo usuário na interface da aplicação
+
+            subtrai1 = double.Parse(textBox1.Text); // Recebe valor referente a textBox1 do formulário e converte uma informação tipo Texto para Double, pois a variavel é do tipo double, ou seja, pode receber numeros (Inteiros, Fracionados, Positivos ou Negativos)
+
+            subtrai2 = double.Parse(textBox2.Text); // Recebe valor referente a textBox2 do formulário e converte uma informação tipo Texto para Double, pois a variavel é do tipo double, ou seja, pode receber numeros (Inteiros, Fracionados, Positivos ou Negativos)
+
+            //Efetuando o calculo de Adição
+            resultado = subtrai1 - subtrai2; //Executa o Calculo
+
+            //Mostrar o resultado ao usuário
+            textBox3.Text = resultado.ToString(); //Converte o valor da variavel do tipo double, para texto e assim será possivel retornar o valor dentro uma textBox.
+
+            //Mostrar a operação matemática executada dentro da Label
+            label5.Text = " - ";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Declaração de Variáveis para realização dos calculos
+            double mult1;
+            double mult2;
+            double resultado;
+
+            //Recebendo valores informados pelo usuário na interface da aplicação
+
+            mult1 = double.Parse(textBox1.Text); // Recebe valor referente a textBox1 do formulário e converte uma informação tipo Texto para Double, pois a variavel é do tipo double, ou seja, pode receber numeros (Inteiros, Fracionados, Positivos ou Negativos)
+
+            mult2 = double.Parse(textBox2.Text); // Recebe valor referente a textBox2 do formulário e converte uma informação tipo Texto para Double, pois a variavel é do tipo double, ou seja, pode receber numeros (Inteiros, Fracionados, Positivos ou Negativos)
+
+            //Efetuando o calculo de Adição
+            resultado = mult1 * mult2; //Executa o Calculo
+
+            //Mostrar o resultado ao usuário
+            textBox3.Text = resultado.ToString(); //Converte o valor da variavel do tipo double, para texto e assim será possivel retornar o valor dentro uma textBox.
+
+            //Mostrar a operação matemática executada dentro da Label
+            label5.Text = " * ";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //Declaração de Variáveis para realização dos calculos
+            double div1;
+            double div2;
+            double resultado;
+
+            //Recebendo valores informados pelo usuário na interface da aplicação
+
+            div1 = double.Parse(textBox1.Text); // Recebe valor referente a textBox1 do formulário e converte uma informação tipo Texto para Double, pois a variavel é do tipo double, ou seja, pode receber numeros (Inteiros, Fracionados, Positivos ou Negativos)
+
+            div2 = double.Parse(textBox2.Text); // Recebe valor referente a textBox2 do formulário e converte uma informação tipo Texto para Double, pois a variavel é do tipo double, ou seja, pode receber numeros (Inteiros, Fracionados, Positivos ou Negativos)
+
+            //Efetuando o calculo de Adição
+            resultado = div1 / div2; //Executa o Calculo
+
+            //Mostrar o resultado ao usuário
+            textBox3.Text = resultado.ToString(); //Converte o valor da variavel do tipo double, para texto e assim será possivel retornar o valor dentro uma textBox.
+
+            //Mostrar a operação matemática executada dentro da Label
+            label5.Text = " / ";
+        }
     }
 }
